35109f60ad9b49fe5526b123162080c0d0e52f22
Victor Ndaba <mwangindaba@gmail.com>
2022-09-07 18:57:11 +0300

    style: enforced prettier options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: "standard-with-typescript",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-  }
-}
+  rules: {},
+};
