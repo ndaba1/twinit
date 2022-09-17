43970d558e66445659199d66d039be05b33d5b8a
Victor Ndaba <mwangindaba@gmail.com>
2022-09-18 00:53:36 +0300

    feat: added new utility methods

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,42 @@
+import chalk from "chalk";
 import fs from "fs-extra";
+import { createRequire } from "module";
+import path from "path";
 import { DIRECTIVES } from "./constants.js";
+
+const require = createRequire(import.meta.url);
 
 export async function copyDirectives(file: string) {
   // Write tailwind directives to the index.css/globals.css file
+  const directives = DIRECTIVES.trim();
   const data = await fs.readFile(file, "utf8");
-  await fs.writeFile(file, DIRECTIVES.trim() + "\n" + data);
+  if (!data.includes(directives)) {
+    await fs.writeFile(file, directives + "\n\n" + data);
+  }
+}
+
+export async function injectGlob(globs: string[]) {
+  // Import tailwind config file and inject the globs in the `content` field
+  const config = require(path.join(process.cwd(), "tailwind.config.js"));
+  const sources = config.content || [];
+  //  add new sources but only if they don't already exist
+  globs.forEach((glob) => {
+    if (!sources.includes(glob)) {
+      sources.push(glob);
+    }
+  });
+  config.content = sources;
+  await fs.writeFile(
+    path.join(process.cwd(), "tailwind.config.js"),
+    `module.exports = ${JSON.stringify(config, null, 2)}`
+  );
+}
+
+export function showSuccess() {
+  console.log(
+    `${chalk.green(
+      "\n Success!"
+    )} Tailwindcss has been initialized on your project.\n`
+  );
+  console.log(chalk.cyan(" Happy tailwind-ing, I guess!\n"));
 }
