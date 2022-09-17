7dc61dbee8d0805b9c4d4624996e468d0def37aa
Victor Ndaba <mwangindaba@gmail.com>
2022-09-18 02:41:49 +0300

    refactor: simplied CRA implementation

--- src/lib/cra.ts
+++ src/lib/cra.ts
@@ -1,51 +1,19 @@
-import chalk from "chalk";
-import { execa } from "execa";
-import fs from "fs-extra";
-import { Listr } from "listr2";
-import path from "path";
-import { copyDirectives, injectGlob, showSuccess } from "../util/index.js";
-import detectPackageManager from "../util/pacman.js";
+import {
+  getCssFilePath,
+  getGenericTasks,
+  injectGlob,
+  showSuccess,
+} from "../util/index.js";
 
 export default async function start() {
-  const pacman = await detectPackageManager();
-
-  console.log(
-    chalk.cyan(`Using ${chalk.yellow(pacman.name)} as package manager`)
-  );
-  // setup listr tasks
-  const tasks = new Listr([
-    {
-      title: "Installing dependencies...",
-      task: async () =>
-        await pacman.install(["tailwindcss", "postcss", "autoprefixer"]),
+  const tasks = await getGenericTasks(await getCssFilePath());
+  tasks.add({
+    title: "Adding content sources...",
+    task: async () => {
+      await injectGlob(["./src/**/*.{js,jsx,ts,tsx}"], "tailwind.config.js");
     },
-    {
-      title: "Initializing tailwind config...",
-      task: async () => await execa("npx", ["tailwindcss", "init", "-p"]),
-    },
-    {
-      title: "Copying tailwind directives...",
-      task: async () => await copyDirectives(getCssFile()),
-    },
-    {
-      title: "Adding content sources...",
-      task: async () => {
-        await injectGlob(["./src/**/*.{js,jsx,ts,tsx}"]);
-      },
-    },
-  ]);
+  });
 
   await tasks.run();
   showSuccess();
-}
-
-function getCssFile() {
-  const files = ["index.css", "global.css", "styles.css"];
-  for (const file of files) {
-    const p = path.join(process.cwd(), "src", file);
-    if (fs.existsSync(p)) {
-      return p;
-    }
-  }
-  return null;
 }
