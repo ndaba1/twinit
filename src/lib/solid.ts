8b3c90c9dadc9edb61a647167138b76fd155f681
Victor Ndaba <mwangindaba@gmail.com>
2022-09-18 02:42:19 +0300

    chore: fixed comment

--- src/lib/solid.ts
+++ src/lib/solid.ts
@@ -1,4 +1,4 @@
-// Implementation for solid is exactly the same as for react
+// Implementation for solid is exactly the same as for CRA
 import implementation from "./cra.js";
 
 export default implementation;
