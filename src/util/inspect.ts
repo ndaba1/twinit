85136ce960b29177e9805e0c350d12c30d55fd42
Victor Ndaba <mwangindaba@gmail.com>
2022-09-17 22:42:47 +0300

    refactor: added function to resolve framework

--- src/util/inspect.ts
+++ src/util/inspect.ts
@@ -2,8 +2,12 @@
 // If inspecting package.json fails, ask the user to provide the framework
 // If successful, run the setup for the framework
 
+import fs from "fs";
 import inquirer from "inquirer";
+import { createRequire } from "module";
 import path from "path";
+
+const require = createRequire(import.meta.url);
 
 // A list of common frameworks and their package.json keys
 const frameworks = {
@@ -24,15 +28,17 @@ export async function detectFramework() {
 
   // Wrap in try/catch in case the file does not exist
   try {
-    const pkg = await import(`file://${file}`);
-    console.log(pkg);
+    console.log("Trying to detect framework from package.json");
+    // let pkg = await import(`file://${file}`, { assert: { type: "json" } });
+    const pkg = require(file);
 
     Object.keys(frameworks).forEach((fw) => {
       const keys: string[] = frameworks[fw];
       const found = keys.some((key) => pkg.dependencies[key] !== undefined);
       if (found) {
         console.log(`Found ${fw}`);
-        framework = fw;
+        // Check if fw is react and has vite
+        framework = checkFramework(fw);
       }
     });
   } catch (error) {
@@ -43,15 +49,40 @@ export async function detectFramework() {
     return framework;
   }
 
+  // TODO: Try to detect the framework from the files in the directory
+
   // If no framework is found, prompt the user to choose one
   const fw = await inquirer.prompt([
     {
       type: "list",
       name: "framework",
+      required: true,
       message: "Failed to detect a framework. Please choose one:",
       choices: Object.keys(frameworks),
     },
   ]);
 
-  return fw.framework;
+  return checkFramework(fw.framework);
+}
+
+function fileExists(file: string) {
+  try {
+    return fs.existsSync(path.join(process.cwd(), file));
+  } catch (error) {
+    return false;
+  }
+}
+
+function checkFramework(fw: string) {
+  if (fw === "react" && fileExists("vite.config.js")) {
+    return "vite-react";
+  }
+  if (fw === "vue" && fileExists("vite.config.js")) {
+    return "vite-vue";
+  }
+  if (fw === "react") {
+    return "cra";
+  }
+
+  return fw;
 }
