3068863f3202352e9ade63f8f58e58e494e9b245
Victor Ndaba <mwangindaba@gmail.com>
2022-09-19 00:47:24 +0300

    feat: added list cmd and synced with pkgJson
    updated error messages
    
     added `twinit list` cmd
    
     synced package name,

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,34 @@
 import chalk from "chalk";
 import { program } from "commander";
 import fs from "fs-extra";
+import { createRequire } from "module";
 import path from "path";
 import { fileURLToPath } from "url";
+import { FW_DEPS, FW_FILES } from "./util/constants.js";
 import { detectFramework } from "./util/inspect.js";
 
+const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const { name, version, description } = require(path.join(
+  __dirname,
+  "../",
+  "package.json"
+));
 
 program
-  .name("twinit")
-  .version("0.1.0")
-  .description("A CLI for setting up tailwindcss on your framework of choice")
+  .name(name)
+  .version(version)
+  .description(description)
   .argument("[framework]", "The framework you are using")
   .action(setup);
+
+program
+  .command("list")
+  .description("List the supported frameworks")
+  .action(() => {
+    console.log([...Object.keys(FW_DEPS), ...Object.keys(FW_FILES)].join("\n"));
+  });
 
 async function setup(fw: string) {
   let framework = fw;
@@ -28,8 +43,9 @@ async function setup(fw: string) {
   if (!fs.existsSync(file)) {
     console.log(
       chalk.red(
-        `Framework not supported! An implementation for the framework: ${framework} could not be found`
-      )
+        `Framework not supported! An implementation for the framework: ${framework} could not be found.\n `
+      ),
+      "Run: `twinit list` to see a list of the supported frameworks."
     );
     process.exit(1);
   }
