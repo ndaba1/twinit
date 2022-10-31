d4cdbd41f4ac2266e31a684542f82b116fbc84c6
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:51:03 +0300

    refactor: simplified implementations

--- src/lib/vite-react.ts
+++ src/lib/vite-react.ts
@@ -1,22 +1,11 @@
 // Vite-react similar to CRA with very minor differences
-import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
+import { runGenericTasks } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function (options: any) {
-  const tasks = await getGenericTasks(options);
-
-  if (!options.onlyDeps) {
-    tasks.add({
-      title: "Adding content sources...",
-      task: async () => {
-        await injectGlob(
-          ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-          "tailwind.config.cjs"
-        );
-      },
-    });
-  }
-
-  await tasks.run();
-  showSuccess();
+export default async function (opts: any) {
+  await runGenericTasks({
+    opts,
+    sources: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    twFile: "tailwind.config.cjs",
+  });
 }
--- src/lib/vite-vue.ts
+++ src/lib/vite-vue.ts
@@ -1,19 +1,10 @@
-import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
+import { runGenericTasks } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function (options: any) {
-  const tasks = await getGenericTasks(options);
-
-  tasks.add({
-    title: "Adding content sources...",
-    task: async () => {
-      await injectGlob(
-        ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-        "tailwind.config.cjs"
-      );
-    },
+export default async function (opts: any) {
+  await runGenericTasks({
+    opts,
+    sources: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    twFile: "tailwind.config.cjs",
   });
-
-  await tasks.run();
-  showSuccess();
 }
