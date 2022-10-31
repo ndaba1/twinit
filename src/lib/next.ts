5cfb79c0e634f273dcc8cfbc10f29f34d670aaa4
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:50:33 +0300

    refactor: updated next implementation

--- src/lib/next.ts
+++ src/lib/next.ts
@@ -1,22 +1,13 @@
 import fs from "fs-extra";
 import path from "path";
-import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
+import { runGenericTasks } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function start(options: any) {
-  const tasks = await getGenericTasks(options);
-
-  if (!options.onlyDeps) {
-    tasks.add({
-      title: "Adding content sources...",
-      task: async () => {
-        await injectGlob(getSources(), "tailwind.config.js");
-      },
-    });
-  }
-
-  await tasks.run();
-  showSuccess();
+export default async function start(opts: any) {
+  await runGenericTasks({
+    opts,
+    sources: getSources(),
+  });
 }
 
 function getSources() {
