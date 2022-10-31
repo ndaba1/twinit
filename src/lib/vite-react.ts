27193fd541b48e2a1db136938dbd0611ebb274d7
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:25:54 +0300

    refactor: updated vite react implementation

--- src/lib/vite-react.ts
+++ src/lib/vite-react.ts
@@ -1,14 +1,9 @@
 // Vite-react similar to CRA with very minor differences
-import {
-  getCssFilePath,
-  getGenericTasks,
-  injectGlob,
-  showSuccess,
-} from "../util/index.js";
+import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function (options: any) {
-  const tasks = await getGenericTasks(await getCssFilePath(), options);
+  const tasks = await getGenericTasks(options);
 
   if (!options.onlyDeps) {
     tasks.add({
