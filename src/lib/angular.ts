7fe12609ef9e1060f05127845655e204771ce0e6
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:49:28 +0300

    refactor: simplified angular implementation

--- src/lib/angular.ts
+++ src/lib/angular.ts
@@ -1,22 +1,11 @@
 import path from "path";
-import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
+import { runGenericTasks } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function (options: any) {
-  const tasks = await getGenericTasks(
-    options,
-    path.join(process.cwd(), "src", "styles.css")
-  );
-
-  if (!options.onlyDeps) {
-    tasks.add({
-      title: "Adding content sources...",
-      task: async () => {
-        await injectGlob(["./src/**/*.{html,ts}"], "tailwind.config.js");
-      },
-    });
-  }
-
-  await tasks.run();
-  showSuccess();
+export default async function (opts: any) {
+  await runGenericTasks({
+    opts,
+    sources: ["./src/**/*.{html,ts}"],
+    cssFile: path.join(process.cwd(), "src", "styles.css"),
+  });
 }
