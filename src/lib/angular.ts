d3565b74ac73dc0c98c5a838543f5706378e4923
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:23:20 +0300

    refactor: updated angular impl to take in options

--- src/lib/angular.ts
+++ src/lib/angular.ts
@@ -1,17 +1,21 @@
 import path from "path";
 import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
 
-export default async function () {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function (options: any) {
   const tasks = await getGenericTasks(
+    options,
     path.join(process.cwd(), "src", "styles.css")
   );
 
-  tasks.add({
-    title: "Adding content sources...",
-    task: async () => {
-      await injectGlob(["./src/**/*.{html,ts}"], "tailwind.config.js");
-    },
-  });
+  if (!options.onlyDeps) {
+    tasks.add({
+      title: "Adding content sources...",
+      task: async () => {
+        await injectGlob(["./src/**/*.{html,ts}"], "tailwind.config.js");
+      },
+    });
+  }
 
   await tasks.run();
   showSuccess();
