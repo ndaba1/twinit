0f74c690bf75940a5ce4c84e9fd581d9eae63ec3
Victor Ndaba <mwangindaba@gmail.com>
2022-10-31 21:50:02 +0300

    refactor: reduced duplication in cra implementation

--- src/lib/cra.ts
+++ src/lib/cra.ts
@@ -1,18 +1,9 @@
-import { getGenericTasks, injectGlob, showSuccess } from "../util/index.js";
+import { runGenericTasks } from "../util/index.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function start(options: any) {
-  const tasks = await getGenericTasks(options);
-
-  if (!options.onlyDeps) {
-    tasks.add({
-      title: "Adding content sources...",
-      task: async () => {
-        await injectGlob(["./src/**/*.{js,jsx,ts,tsx}"], "tailwind.config.js");
-      },
-    });
-  }
-
-  await tasks.run();
-  showSuccess();
+export default async function start(opts: any) {
+  await runGenericTasks({
+    opts,
+    sources: ["./src/**/*.{js,jsx,ts,tsx}"],
+  });
 }
